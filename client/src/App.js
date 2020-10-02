54ee263357fc42f28ec601243bedbb4351b64f46
Paul Lujan <paul.lujan@smilebrands.com>
2020-10-02 11:49:01 -0700

    Form is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [formData, setFormData] = useState({ fname: "", lname: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+  };
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
-    <div>
-      <h1>App.js</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="fname">First name:</label>
+      <br />
+      <input type="text" id="fname" name="fname" value={formData["fname"]} onChange={handleInput} />
+      <br />
+      <label htmlFor="lname">Last name:</label>
+      <br />
+      <input type="text" id="lname" name="lname" value={formData["lname"]} onChange={handleInput} />
+      <br />
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
   );
-}
+};
 
 export default App;
